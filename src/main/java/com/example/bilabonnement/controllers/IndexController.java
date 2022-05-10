92ac400e16bde59cbd33a4d11d1b5f296046be8e
Lauribee <laura_stroe_2001@yahoo.com>
2022-05-10 11:29:31 +0200

    buttons oprettet

--- src/main/java/com/example/bilabonnement/controllers/IndexController.java
+++ src/main/java/com/example/bilabonnement/controllers/IndexController.java
@@ -1,4 +1,35 @@
 package com.example.bilabonnement.controllers;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+    @Controller
+    public class IndexController {
+
+        @GetMapping("/")
+        public String index() {
+            return "index";
+        }
+
+        @GetMapping("/dataregistrering")
+        public String data() {
+            return "dataregistrering";
+        }
+
+        @GetMapping("/skaderegistrering")
+        public String skadereg() {
+            return "skaderegistrering";
+        }
+
+        @GetMapping("/statistik")
+        public String stats() {
+            return "statistik";
+        }
+
+        @GetMapping("/opret-lejeaftale")
+        public String opretLejeaftale() {
+            return "opret-lejeaftale";
+        }
+
 }
