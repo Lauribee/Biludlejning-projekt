de1d80ec7f2c6aa8be3325d4b577fdffadf5130c
Lauribee <laura_stroe_2001@yahoo.com>
2022-05-16 21:17:35 +0200

    opret lejeftale i controller færdig

--- src/main/java/com/example/bilabonnement/controllers/LejeaftaleController.java
+++ src/main/java/com/example/bilabonnement/controllers/LejeaftaleController.java
@@ -7,27 +7,20 @@ import com.example.bilabonnement.repository.LejeaftaleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.sql.SQLException;
 
 @Controller
 public class LejeaftaleController {
 
-    BilController bc = new BilController();
     LejeaftaleRepository lr = new LejeaftaleRepository();
 
     @PostMapping("/lejeaftaler")
-    public String opretLejeaftale(WebRequest dataFromForm, HttpServletRequest request, RequestBody requestBody) throws SQLException {
-        HttpSession session = request.getSession();
+    public String opretLejeaftale(@RequestBody Bil bil, @RequestBody Kunde kunde) {
 
-        Bil bil = (Bil) session.getAttribute("bil");
-        Kunde kunde = (Kunde) session.getAttribute("kunde");
         boolean status = true;
 
         Lejeaftale lejeaftale = new Lejeaftale(bil, kunde, status);
+        System.out.println(lejeaftale.toString());
         lr.indsætLejeaftale(lejeaftale);
         return "redirect:/lejeaftaler";
     }
