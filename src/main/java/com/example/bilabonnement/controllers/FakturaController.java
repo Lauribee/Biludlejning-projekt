cbb1963d95cf1dfc10163fbf7fe00e4596e4f8c5
chri75ky <chri75ky@stud.kea.dk>
2022-05-23 16:46:10 +0200

    Startet på faktura

--- src/main/java/com/example/bilabonnement/controllers/FakturaController.java
+++ src/main/java/com/example/bilabonnement/controllers/FakturaController.java
@@ -2,12 +2,13 @@ package com.example.bilabonnement.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class FakturaController {
 
-    @PostMapping("/skaderegistrering")
-    public String registrerSkader() {
+    @PostMapping("/index")
+    public String fakturaOprettelse(WebRequest dataFraForm) {
 
         return "";
     }
