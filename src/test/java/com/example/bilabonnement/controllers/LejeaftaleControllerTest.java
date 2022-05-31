55f3fba79f37558e77a8b6b568e98418bca34d4b
Beeboo <laura_stroe_2001@yahoo.com>
2022-05-31 03:09:39 +0200

    unit test

---
+++ src/test/java/com/example/bilabonnement/controllers/LejeaftaleControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.bilabonnement.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LejeaftaleControllerTest {
+
+    @Test
+    void opretLejeaftale() {
+        //Arrange
+
+        //Act
+        //Assert
+    }
+
+    @Test
+    void lejeaftaler() {
+    }
+
+    @Test
+    void ændreStatus() {
+    }
+}
---
+++ src/test/java/com/example/bilabonnement/services/FakturaServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.bilabonnement.services;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FakturaServiceTest {
+
+    @Test
+    void skabFakturaSkalReturnereEtRigtigtBeløb() {
+        var f = new FakturaService();
+
+    }
+}
